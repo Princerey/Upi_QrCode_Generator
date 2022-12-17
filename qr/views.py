24cdfaf310919d468e33ca919d7f6abd8d8a3287
Princerey <rohkumar0126@gmail.com>
2022-12-18 04:55:10 +0530

    initial commit

---
+++ qr/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+import qrcode
+import qrcode.image.svg
+from io import BytesIO
+
+def index(request):
+    context = {}
+    if request.method == "POST":
+        factory = qrcode.image.svg.SvgImage
+        img = qrcode.make("upi://pay?pa="+request.POST.get("qr_text","")+"&pn="+request.POST.get("qr_text1",""), image_factory=factory, box_size=20)
+        stream = BytesIO()
+        img.save(stream)
+        context["svg"] = stream.getvalue().decode()
+
+    return render(request, "homepage.html", context=context)
